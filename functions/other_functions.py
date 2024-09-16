d79d3456d4daa1b7739a5ca8a588e7837e950419
renoaldo <renoaldo@hotmail.com>
2024-09-16 10:56:48 -0300

    fix image path

--- functions/other_functions.py
+++ functions/other_functions.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 def banner():
-    st.image('src\images\Banner Website Imobiliária Moderno Verde Marrom2.png', use_column_width=True)
+    st.image('./src/images/Banner Website Imobiliária Moderno Verde Marrom2.png', use_column_width=True)
     
